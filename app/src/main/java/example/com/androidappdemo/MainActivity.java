659615578486b8d391a999da8146b4b51b14946b
raveena25 <raveena.nagdev@ves.ac.in>
2018-01-11 13:29:47 +0530

    Create MainActivity.java

---
+++ app/src/main/java/example/com/androidappdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package example.com.androidappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.text);
+    }
+}
